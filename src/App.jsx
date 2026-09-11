c3e31516350cfd82151ec478dff842ce7dff36a2
motesaivignesh <motesaivignesh@gmail.com>
2026-09-11 22:55:27 +0530

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import Card from './Card.jsx'
+import Student from './Student.jsx'
 function App() {
   return( 
     <>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
+    <Student name="Vignesh" age={20} isStudent={true}/>
+    <Student name="Bubby" age={19} isStudent={true}/>
+    <Student name="puppy" age={24} isStudent={false}/>
     </>
   );
 }
